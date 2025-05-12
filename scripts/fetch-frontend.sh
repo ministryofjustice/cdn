4e5cf4445521109052a694df40eda1c47d8ddb6e
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-05-12 21:35:56 +0000

    fix: update frontend options and streamline directory creation in fetch script
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- scripts/fetch-frontend.sh
+++ scripts/fetch-frontend.sh
@@ -7,31 +7,38 @@ FRONTEND_VERSION="${2}"
 
 # Check if FRONTEND is valid
 case "${FRONTEND}" in
-  "govuk")
+  "govuk-frontend")
     REPOSITORY="alphagov/govuk-frontend"
+    RELEASE_PREFIX="release"
   ;;
-  "moj")
+  "moj-frontend")
     REPOSITORY="ministryofjustice/moj-frontend"
+    RELEASE_PREFIX="release"
   ;;
   *)
     echo "Unknown frontend: ${FRONTEND}"
-    echo "Valid options are: govuk, moj"
+    echo "Valid options are: govuk-frontend, moj-frontend"
     exit 1
   ;;
 esac
 
 # Check if FRONTEND_VERSION is valid
+if [[ "$(gh release list --repo "${REPOSITORY}" --exclude-pre-releases --json tagName | jq -r --arg tag "${FRONTEND_VERSION}" '.[].tagName | select(. == $tag)')" != "${FRONTEND_VERSION}" ]]; then
+  echo "Invalid version: ${FRONTEND_VERSION}"
+  echo "Please check available versions on GitHub: https://github.com/${REPOSITORY}/releases"
+  exit 1
+fi
 
+# Create directories
+mkdir --parents "dist/${FRONTEND}/${FRONTEND_VERSION}"
+mkdir --parents "tmp/${FRONTEND}/${FRONTEND_VERSION}"
 
+# Download artifact
+gh release download \
+  --repo "${REPOSITORY}" \
+  --pattern "${RELEASE_PREFIX}-${FRONTEND_VERSION}.zip" \
+  --dir "tmp/${FRONTEND}/${FRONTEND_VERSION}" \
+  --clobber
 
-
-#######################
-
-# mkdir --parents "dist/${FRONTEND}/${FRONTEND_VERSION}"
-# mkdir --parents "tmp/${FRONTEND}/${FRONTEND_VERSION}"
-
-# # https://github.com/alphagov/govuk-frontend/releases/download/v5.10.0/release-v5.10.0.zip
-# # https://github.com/ministryofjustice/moj-frontend/releases/download/v5.1.3/release-v5.1.3.zip
-
-# curl --location "https://github.com/ministryofjustice/moj-frontend/releases/download/v5.1.3/release-v5.1.3.zip" \
-#   --output "tmp/${FRONTEND}/${FRONTEND_VERSION}/release.zip"
+# Unarchive artifact
+unzip -o "tmp/${FRONTEND}/${FRONTEND_VERSION}/${RELEASE_PREFIX}-${FRONTEND_VERSION}.zip" -d "dist/${FRONTEND}/${FRONTEND_VERSION}"
